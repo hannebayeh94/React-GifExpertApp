cdf9613b02642d13b96f2328be797602539f178c
hanne bayeh <hannejosebayehvillarreal@gmail.com>
2021-09-22 11:56:49 -0500

    docs agregado

---
+++ docs/precache-manifest.b7b65cab19d987823ecf8cd5d2f9f31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd3a979e8c5238a09925a5a41193609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "613b34e9107f1c127d86",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613b34e9107f1c127d86",
+    "url": "/static/js/main.d192b68c.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
